d823af19a7eeea9a8063c78281e252b9657f35ba
rddaps <dapa0002@umn.edu>
2023-10-05 22:19:19 -0400

    implementing workbox plugins, asset caching and css loaders + babel

--- client/src-sw.js
+++ client/src-sw.js
@@ -24,11 +24,16 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache)
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-;
-
-// TODO: Implement asset caching
-registerRoute();
-
-new 
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
